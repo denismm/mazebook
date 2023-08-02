ab3a99562cf02907d5f5c45a3724df6294271f31
Denis Moskowitz <dmoskowi@akamai.com>
2023-08-02 16:54:22 -0400

    add wilson algorithm

---
+++ wilson.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+from maze import make_wilson
+
+maze_width = int(sys.argv[1])
+maze_height = int(sys.argv[2])
+
+grid = make_wilson(maze_height, maze_width)
+
+long_path = grid.longest_path()
+field = grid.dijkstra(long_path[0])
+grid.ascii_print(field=field, path=long_path)

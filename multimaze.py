bd0735179d3a240b04002ec762183239aa778f82
Denis Moskowitz <dmoskowi@akamai.com>
2023-08-03 14:07:26 -0400

    sketch of general script

---
+++ multimaze.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import maze
+import sys
+import argparse
+
+# size
+width = 10
+height = 10
+# algorithm
+algorithm = 'wilson'
+# output
+output = 'png'
+output_name = 'wilson'
+# field?
+include_field = True
+# path?
+include_path = True
+
+seed

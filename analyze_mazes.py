7d6df08574a4d6ab94a5782a120d85a0d690b578
Denis Moskowitz <denismm@gmail.com>
2023-08-08 11:14:34 -0400

    add time taken in analysis, improve format

--- analyze_mazes.py
+++ analyze_mazes.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import maze
+import time
 
 SIZE = 50
 SAMPLES = 50
 
 analysis_per_algorithm: dict[str, dict[int, float]] = {}
 
+print("\t".join([str(x) for x in range(1, 5)] + ['ms', 'algorithm']))
 for algorithm in maze.Grid.algorithms.keys():
+    start = time.time()
     all_data: dict[int, int] = {k: 0 for k in range(5)}
     for _ in range(SAMPLES):
         g = maze.Grid(SIZE, SIZE)
@@ -14,7 +17,15 @@ for algorithm in maze.Grid.algorithms.keys():
         analysis = g.node_analysis()
         for k, v in analysis.items():
             all_data[k] += v
-    analysis_per_algorithm[algorithm] = {k: v / SAMPLES for k, v in all_data.items()}
-    print("\t".join([ str(analysis_per_algorithm[algorithm][k]) for k in range(5)]), algorithm)
+    span = (time.time() - start) * 1000 / SAMPLES
+    analysis_per_algorithm[algorithm] = {
+        k: 100 * v / (SAMPLES * SIZE * SIZE) for k, v in all_data.items()
+    }
+    output = [
+        str(analysis_per_algorithm[algorithm][k]) for k in range(1, 5)
+    ]
+    output.append(str(int(span)))
+    output.append(algorithm)
+    print("\t".join(output))
 
 # print(analysis_per_algorithm)

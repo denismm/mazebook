ef899d3f61ad2ed484bf751281476d7a8cce8e78
Denis Moskowitz <denismm@gmail.com>
2023-12-28 16:51:18 -0500

    add simple test

---
+++ tests/rect_test.py
@@ -0,0 +1,31 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from grid.rectgrid import RectGrid
+import random
+
+def test_rect() -> None:
+    random.seed(97)
+    small_grid = RectGrid(3, 4)
+    assert len(small_grid) == 12
+    small_grid.generate_maze('binary')
+    structure = small_grid.structured_data()
+    assert structure['self'] == 'rectmaze'
+    assert structure['width'] == 4
+    assert structure['height'] == 3
+    cells = structure['cells']
+    assert cells == [
+        {'position': [0, 0], 'links': [[1, 0]]},
+        {'position': [0, 1], 'links': [[1, 1]]},
+        {'position': [0, 2], 'links': [[1, 2]]},
+        {'position': [1, 0], 'links': [[0, 0], [1, 1]]},
+        {'position': [1, 1], 'links': [[0, 1], [1, 0], [2, 1]]},
+        {'position': [1, 2], 'links': [[0, 2], [2, 2]]},
+        {'position': [2, 0], 'links': [[2, 1]]},
+        {'position': [2, 1], 'links': [[1, 1], [2, 0], [3, 1]]},
+        {'position': [2, 2], 'links': [[1, 2], [3, 2]]},
+        {'position': [3, 0], 'links': [[3, 1]]},
+        {'position': [3, 1], 'links': [[2, 1], [3, 0], [3, 2]]},
+        {'position': [3, 2], 'links': [[2, 2], [3, 1]]}
+    ]

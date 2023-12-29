34877605be046d395965c4901b76af9c02a47902
Denis Moskowitz <denismm@gmail.com>
2023-12-29 16:30:42 -0500

    use more namedtuples

--- grid/multigrid.py
+++ grid/multigrid.py
@@ -1,21 +1,21 @@
 # grids with cells in a square layout
 
 from positions import Position, IntPosition, Direction, cardinal_directions, add_direction, manhattan, Coordinates
-from typing import Optional, Any, Callable, Sequence
+from typing import Optional, Any, Callable, Sequence, NamedTuple
 import random
 
 from .maze import BaseGrid, ps_list
 
-EdgeSpec = tuple[
-    str,                # target grid
-    int,                # target side (1-based)
-    bool,               # whether to flip edge connection
-]
-GridSpec = tuple[
-    type[BaseGrid],                     # type of subgrid
-    tuple[int|bool, ...],               # args to subgrid init
-    tuple[Optional[EdgeSpec], ...]      # target edge of each edge
-]
+EdgeSpec = NamedTuple('EdgeSpec', [
+    ('target', str),               # target grid
+    ('side', int),                # target side
+    ('flip', bool),               # whether to flip edge connection
+])
+GridSpec = NamedTuple('GridSpec', [
+    ('grid_class', type[BaseGrid]),             # type of subgrid
+    ('args', tuple[int|bool, ...]),             # args to subgrid init
+    ('edges', tuple[Optional[EdgeSpec], ...]),  # target edge of each edge
+])
 
 class MultiGrid(BaseGrid):
     def __init__(self, 
@@ -23,18 +23,17 @@ class MultiGrid(BaseGrid):
         **kwargs: Any,
     ) -> None:
         super().__init__(**kwargs)
-        self._subgrids: dict[BaseGrid] = {}
+        self._subgrids: dict[str, BaseGrid] = {}
         for grid_name, grid_spec in subgrids.items():
-            grid_class, grid_args, edges = grid_spec
-            self._subgrids[grid_name] = grid_class(
-                *grid_args,
+            self._subgrids[grid_name] = grid_spec.grid_class(
+                *grid_spec.args,
                 grid=self._grid,
                 gridname=grid_name
             )
 
     def pos_adjacents(self, start: Position) -> Sequence[Position]:
         gridname = start.gridname
-        subgrid = self._subgrids[gridname]
+        subgrid = self._subgrids[gridname]      # type: ignore [index]
         neighbors: list[Position] = list(subgrid.pos_adjacents(start))
         neighbors.extend(super().pos_adjacents(start))
         return neighbors

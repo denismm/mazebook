e6942d74925595c9d3132028e471557ee0340de6
Denis Moskowitz <denismm@gmail.com>
2024-01-08 20:28:46 -0500

    pull separate scripts for complex mazes into complex_maze

--- grid/complex_maze.py
+++ grid/complex_maze.py
@@ -76,3 +76,152 @@ def slender_star(size: int, **kwargs: str) -> MultiGrid:
         ),
     }
     return MultiGrid(subgrids, **kwargs)
+
+@complex
+def fat_star(size: int, **kwargs: str) -> MultiGrid:
+    point_kwargs = {'slices': 1}
+    subgrids = {
+        "C": GridSpec(
+            PolygonGrid,
+            (size, 5),
+            (
+                EdgeSpec("0", 0, True),
+                EdgeSpec("1", 0, True),
+                EdgeSpec("2", 0, True),
+                EdgeSpec("3", 0, True),
+                EdgeSpec("4", 0, True),
+            ),
+            (0, 0),
+            rotation=54,
+        ),
+        "0": GridSpec(
+            PolygonGrid,
+            (size, 5),
+            (EdgeSpec("C", 0, True, align=True),),
+            kwargs = point_kwargs,
+        ),
+        "1": GridSpec(
+            PolygonGrid,
+            (size, 5),
+            (EdgeSpec("C", 1, True, align=True),),
+            kwargs = point_kwargs,
+        ),
+        "2": GridSpec(
+            PolygonGrid,
+            (size, 5),
+            (EdgeSpec("C", 2, True, align=True),),
+            kwargs = point_kwargs,
+        ),
+        "3": GridSpec(
+            PolygonGrid,
+            (size, 5),
+            (EdgeSpec("C", 3, True, align=True),),
+            kwargs = point_kwargs,
+        ),
+        "4": GridSpec(
+            PolygonGrid,
+            (size, 5),
+            (EdgeSpec("C", 4, True, align=True),),
+            kwargs = point_kwargs,
+        ),
+    }
+
+    return MultiGrid(subgrids, **kwargs)
+
+@complex
+def cube(size: int, **kwargs: str) -> MultiGrid:
+    subgrids = {
+        "F": GridSpec(
+            RectGrid,
+            (size, size),
+            (
+                EdgeSpec("R", 2, True),
+                EdgeSpec("U", 3, True),
+                EdgeSpec("L", 0, True),
+                EdgeSpec("D", 1, True),
+            ),
+        ),
+        "B": GridSpec(
+            RectGrid,
+            (size, size),
+            (
+                EdgeSpec("R", 0, True),
+                EdgeSpec("D", 3, True, align=True),
+                EdgeSpec("L", 2, True),
+                EdgeSpec("U", 1, True),
+            ),
+        ),
+        "L": GridSpec(
+            RectGrid,
+            (size, size),
+            (
+                EdgeSpec("F", 2, True, align=True),
+                EdgeSpec("U", 2, True),
+                EdgeSpec("B", 2, True),
+                EdgeSpec("D", 2, True),
+            ),
+        ),
+        "R": GridSpec(
+            RectGrid,
+            (size, size),
+            (
+                EdgeSpec("B", 0, True),
+                EdgeSpec("U", 0, True),
+                EdgeSpec("F", 0, True, align=True),
+                EdgeSpec("D", 0, True),
+            ),
+        ),
+        "U": GridSpec(
+            RectGrid,
+            (size, size),
+            (
+                EdgeSpec("R", 1, True),
+                EdgeSpec("B", 3, True),
+                EdgeSpec("L", 1, True),
+                EdgeSpec("F", 1, True, align=True),
+            ),
+        ),
+        "D": GridSpec(
+            RectGrid,
+            (size, size),
+            (
+                EdgeSpec("R", 3, True),
+                EdgeSpec("F", 3, True, align=True),
+                EdgeSpec("L", 3, True),
+                EdgeSpec("B", 1, True),
+            ),
+        ),
+    }
+
+    return MultiGrid(subgrids, **kwargs)
+
+@complex
+def heart(size: int, **kwargs: str) -> MultiGrid:
+    subgrids = {
+        "C": GridSpec(
+            RectGrid,
+            (size, size),
+            (
+                EdgeSpec("L", 0, True),
+                EdgeSpec("R", 0, True),
+                None,
+                None,
+            ),
+            rotation=45,
+        ),
+        "L": GridSpec(
+            SemiCircleGrid,
+            (size,),
+            (
+                EdgeSpec("C", 0, True, align=True),
+            ),
+        ),
+        "R": GridSpec(
+            SemiCircleGrid,
+            (size,),
+            (
+                EdgeSpec("C", 1, True, align=True),
+            ),
+        ),
+    }
+    return MultiGrid(subgrids, **kwargs)
